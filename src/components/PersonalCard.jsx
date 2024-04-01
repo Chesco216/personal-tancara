2c29b7994e53ad98171a810a666a67ce4c8952ec
Chesco216 <franchescofv198@gmail.com>
2024-03-31 23:29:58 -0400

    yiaaaa, asi de picante?

--- src/components/PersonalCard.jsx
+++ src/components/PersonalCard.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import './PersonalCard.css'
 
 export const PersonalCard = ({ id, name }) => {
+
+  const deleteUser = async ( event ) => {
+    const id = event.target.id
+    await fetch(`http://127.0.0.1:3000/personal/${id}`, { method: 'DELETE' })
+  }
+
   return (
     <div className='card-container'>
-      <label>{id}</label>
-      <label>{name}</label>
+      <label className='card-label'>{id} :</label>
+      <label className='card-label'>{name}</label>
+      <button onClick={ deleteUser } id={id} className='delete-user-button'>x</button>
     </div>
   )
 } 
